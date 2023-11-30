3f453290b0f941e787ace28579acc0de135c6f22
Jang <sdsd090811@gmail.com>
2023-11-30 21:15:27 +0900

    fix: 컨트롤러에서 Gpt 모듈을 사용하도록 수정

--- src/main/java/com/mock/interview/contorller/ChatController.java
+++ src/main/java/com/mock/interview/contorller/ChatController.java
@@ -1,40 +1,32 @@
 package com.mock.interview.contorller;
 
-import com.mock.interview.gpt.ChatRequest;
-import com.mock.interview.gpt.ChatResponse;
+import com.mock.interview.contorller.dto.InterviewInfo;
+import com.mock.interview.gpt.ChatGPTRequester;
+import com.mock.interview.gpt.dto.ChatGptResponse;
+import com.mock.interview.contorller.dto.Message;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class ChatController {
-    
-    @Qualifier("openaiRestTemplate")
-    @Autowired
-    private RestTemplate restTemplate;
-    
-    @Value("${openai.model}")
-    private String model;
-    
-    @Value("${openai.api.url}")
-    private String apiUrl;
-    
+
+    private final ChatGPTRequester requester;
+
     @PostMapping("/chat")
     public String chat(@RequestBody String message) {
-        ChatRequest request = new ChatRequest(model, message);
-        log.info("요청 : {}", request);
+//        MessageHistory history = new MessageHistory(message);
+//        ChatResponse chatResponse = requester.sendRequest(history);
+//        return chatResponse.getChoices().get(0).getMessage().getContent();
+        return "Hello World!";
+    }
 
-        ChatResponse response = restTemplate.postForObject(apiUrl, request, ChatResponse.class);
-        log.info("응답 : {}", response);
-
-        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
-            return "No response";
-        }
-        
-        return response.getChoices().get(0).getMessage().getContent();
+    @PostMapping("/interview/response")
+    public Message chat(@RequestBody InterviewInfo interviewInfo) {
+        // TODO: interviewInfo에 system 환경 설정 정보가 없으면 bad Request를 반환할 것.
+        return requester.sendRequest(interviewInfo);
     }
 }
+
--- src/main/java/com/mock/interview/contorller/MyTestController.java
+++ src/main/java/com/mock/interview/contorller/MyTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MyTestController {
     @GetMapping("/interview/start")
     public String talkPage() {
+        // TODO: GPT에게 첫 요청을 보내고 modle에 Message InterviewInfo를 설정해줄 것
         return "interview/interview-start";
     }
 }
