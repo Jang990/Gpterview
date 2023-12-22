06f9e18f8155193755325533d0d273bc3caeec9e
Jang <sdsd090811@gmail.com>
2023-12-22 21:43:38 +0900

    feat: 면접 진행도 파악을 위한 InterivewProgress 레코드 추가

---
+++ src/main/java/com/mock/interview/infrastructure/interview/strategy/stage/InterviewProgress.java
@@ -0,0 +1,24 @@
+package com.mock.interview.infrastructure.interview.strategy.stage;
+
+/**
+ * 면접 진행 상황에 대한 레코드
+ * @param stage 현재 단계
+ * @param progress 현재 단계의 진행도   ex) 33.4%, 50%...
+ */
+public record InterviewProgress(InterviewStage stage, double progress) {
+    public InterviewProgress(InterviewStage stage, double progress) {
+        if (progress > 100d)
+            throw new IllegalArgumentException(); // TODO: 퍼센트만 받도록.
+
+        this.stage = stage;
+        this.progress = Math.round(progress);
+    }
+
+    @Override
+    public String toString() {
+        return "InterviewProgress{" +
+                "stage=" + stage +
+                ", progress=" + progress +
+                '}';
+    }
+}
