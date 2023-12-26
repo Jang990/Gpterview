86b9833510239164310f727e6ab92c6b2a4e646b
Jang <sdsd090811@gmail.com>
2023-12-26 22:04:47 +0900

    fix: 특정 기술을 선정해서 질문하도록 수정

--- src/main/java/com/mock/interview/infrastructure/interview/strategy/ITInterviewerStrategy.java
+++ src/main/java/com/mock/interview/infrastructure/interview/strategy/ITInterviewerStrategy.java
@@ -15,7 +15,10 @@ import com.mock.interview.presentation.dto.InterviewSettingDto;
 import com.mock.interview.infrastructure.interview.dto.Message;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -25,6 +28,7 @@ public class ITInterviewerStrategy implements InterviewerStrategy {
     private final InterviewSettingCreator interviewSettingCreator;
     private final ITInterviewConcept interviewConcept;
 
+    private final List<String> basicKnowledge = List.of("운영체제", "네트워크", "데이터베이스", "자료구조", "알고리즘");
     private final String[] SUPPORTED_DEPARTMENT = {Category.IT.getName(), "개발"};
 
     @Override
@@ -35,8 +39,25 @@ public class ITInterviewerStrategy implements InterviewerStrategy {
         List<Message> messageHistory = history.getMessages();
         // TODO: AI에 request 토큰 제한이 있기 때문에 message List에서 필요한 부분만 추출해서 넣어야 함.
 
+        if (currentProgress.stage() == InterviewStage.TECHNICAL) {
+            // TODO: 여기서 변경을 가하기 때문에 InterviewSettingDto를 다른 객체로 변경하는 것을 고려해봐야함.
+            String selectedSkill = selectSkillBasedOnProgress(currentProgress.progress(), interviewSettingDto.getProfile().getSkills());
+            interviewSettingDto.getProfile().setSkills(selectedSkill);
+        }
+
         InterviewSetting setting = interviewSettingCreator.create(aiSpec, interviewSettingDto.getProfile(), rawStrategy);
         return new InterviewAIRequest(messageHistory, setting);
+    }
+
+    private String selectSkillBasedOnProgress(double progress, String candidateSkills) {
+        List<String> questionTopicList = new ArrayList<>(basicKnowledge);
+        if (StringUtils.hasText(candidateSkills)) {
+            String[] skills = candidateSkills.trim().split(" ");
+            questionTopicList.addAll(Arrays.asList(skills));
+        }
+
+        int n = (int) Math.floor(progress * questionTopicList.size());
+        return questionTopicList.get(n);
     }
 
     @Override
@@ -47,6 +68,12 @@ public class ITInterviewerStrategy implements InterviewerStrategy {
 
         List<Message> messageHistory = history.getMessages();
         // TODO: AI에 request 토큰 제한이 있기 때문에 message List에서 필요한 부분만 추출해서 넣어야 함.
+
+        if (currentProgress.stage() == InterviewStage.TECHNICAL) {
+            // TODO: 여기서 변경을 가하기 때문에 InterviewSettingDto를 다른 객체로 변경하는 것을 고려해봐야함.
+            String selectedSkill = selectSkillBasedOnProgress(currentProgress.progress(), interviewSettingDto.getProfile().getSkills());
+            interviewSettingDto.getProfile().setSkills(selectedSkill);
+        }
 
         InterviewSetting setting = interviewSettingCreator.create(aiSpec, interviewSettingDto.getProfile(), rawStrategy);
         return new InterviewAIRequest(messageHistory, setting);
