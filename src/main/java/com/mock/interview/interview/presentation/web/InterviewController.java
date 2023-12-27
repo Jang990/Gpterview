1f4228c32a5ecdc4237881f78670b7066a6c3d0f
Jang <sdsd090811@gmail.com>
2023-12-27 22:21:08 +0900

    refactor: SpeechController -> InterviewController 이름 변경

--- src/main/java/com/mock/interview/interview/presentation/web/InterviewController.java
+++ src/main/java/com/mock/interview/interview/presentation/web/InterviewController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class SpeechController {
+public class InterviewController {
 
     @GetMapping("/interview/start")
     public String startInterviewPage(
