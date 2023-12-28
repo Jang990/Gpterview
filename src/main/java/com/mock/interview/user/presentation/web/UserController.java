ec12c6c4f9bebcee380feacbe1d23d46a49185d6
Jang <sdsd090811@gmail.com>
2023-12-28 21:12:27 +0900

    feat: 회원가입 기능 추가

--- src/main/java/com/mock/interview/user/presentation/web/UserController.java
+++ src/main/java/com/mock/interview/user/presentation/web/UserController.java
@@ -1,22 +1,47 @@
 package com.mock.interview.user.presentation.web;
 
+import com.mock.interview.user.application.UserService;
+import com.mock.interview.user.presentation.dto.AccountDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
     @GetMapping("auth/login")
-    public String login() {
+    public String loginPage(Model model) {
+        model.addAttribute("account", new AccountDto());
         return "/auth/login";
     }
 
     @GetMapping("auth/sign-up")
-    public String signUp() {
+    public String signUpPage(Model model) {
+        model.addAttribute("account", new AccountDto("ab","eee"));
         return "/auth/sign-up";
     }
 
     @GetMapping("/")
-    public String index() {
+    public String indexPage() {
         return "index";
+    }
+
+    @PostMapping("auth/sign-up")
+    public String signUp(@Valid @ModelAttribute("account") AccountDto accountDto, BindingResult bindingResult) {
+        System.out.println(bindingResult);
+        if(bindingResult.hasErrors())
+            return "auth/sign-up";
+        return "redirect:/";
+//        userService.create(accountDto);
+//        return "/";
     }
 }
