ead393305877ca14c34b29f994b4db3f5c96aba5
Jang <sdsd090811@gmail.com>
2024-01-04 23:07:08 +0900

    fix: Candidate 컨트롤러 DB 연결 전 임시 코드 제거

--- src/main/java/com/mock/interview/user/presentation/web/CandidateController.java
+++ src/main/java/com/mock/interview/user/presentation/web/CandidateController.java
@@ -15,16 +15,16 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class CandidateController {
-    CandidateProfileService profileService;
+    private final CandidateProfileService profileService;
     @GetMapping("interview/profile")
     public String loadProfileForInterviewPage(Model model, @AuthenticationPrincipal(expression = "id") Long loginId) {
-//        List<CandidateProfileDetailDto> profileList = profileService.findProfiles(loginId);
         model.addAttribute("headerActiveTap", "interview");
+        List<CandidateProfileOverviewDto> profileList = profileService.findProfiles(loginId);
 
-        List<CandidateProfileOverviewDto> profileList = new ArrayList<>();
-        profileList.add(new CandidateProfileOverviewDto(1L, "IT 지원서 1","IT", "백엔드", LocalDateTime.now()));
-        profileList.add(new CandidateProfileOverviewDto(2L, "회계쪽","회계", "회계", LocalDateTime.now()));
-        profileList.add(new CandidateProfileOverviewDto(3L, "IT 지원2","IT", "프론트", LocalDateTime.now()));
+//        List<CandidateProfileOverviewDto> profileList = new ArrayList<>();
+//        profileList.add(new CandidateProfileOverviewDto(1L, "IT 지원서 1","IT", "백엔드", LocalDateTime.now()));
+//        profileList.add(new CandidateProfileOverviewDto(2L, "회계쪽","회계", "회계", LocalDateTime.now()));
+//        profileList.add(new CandidateProfileOverviewDto(3L, "IT 지원2","IT", "프론트", LocalDateTime.now()));
         model.addAttribute("candidateProfileOverviewList", profileList);
         return "/profile/for-interview";
     }
