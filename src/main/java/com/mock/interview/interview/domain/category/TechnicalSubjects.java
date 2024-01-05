b15344dc2f41867c7e8015c568b1f00bd427d950
Jang <sdsd090811@gmail.com>
2024-01-05 21:04:13 +0900

    feat: 기술 엔티티 추가

---
+++ src/main/java/com/mock/interview/interview/domain/category/TechnicalSubjects.java
@@ -0,0 +1,33 @@
+package com.mock.interview.interview.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 기술과 관련 이름 저장
+ * Spring, Prometheus, MySQL, Jenkins, Python 등등
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TechnicalSubjects {
+    @Id
+    @Column(name = "job_category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public static TechnicalSubjects create(String name) {
+        TechnicalSubjects subjects = new TechnicalSubjects();
+        insertName(name, subjects);
+        return subjects;
+    }
+
+    private static void insertName(String name, TechnicalSubjects subjects) {
+        subjects.name = name.toUpperCase();
+    }
+}
