9204dbde568f627ec36474724097bc88c81c6108
Jang <sdsd090811@gmail.com>
2024-01-05 21:00:37 +0900

    fix: CustomClientException을 상속받도록 수정

--- src/main/java/com/mock/interview/user/domain/exception/CandidateProfileNotFoundException.java
+++ src/main/java/com/mock/interview/user/domain/exception/CandidateProfileNotFoundException.java
@@ -1,10 +1,16 @@
 package com.mock.interview.user.domain.exception;
 
+import com.mock.interview.global.exception.CustomClientException;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class CandidateProfileNotFoundException extends RuntimeException {
-    private final HttpStatus status = HttpStatus.NOT_FOUND;
-    private final String code = "candidate.profile.not.found";
+public class CandidateProfileNotFoundException extends CustomClientException {
+    public CandidateProfileNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "candidate.profile.not.found");
+    }
+
+    public CandidateProfileNotFoundException(Throwable cause) {
+        super(cause, HttpStatus.NOT_FOUND, "candidate.profile.not.found");
+    }
 }
--- src/main/java/com/mock/interview/user/domain/exception/UserNotFoundException.java
+++ src/main/java/com/mock/interview/user/domain/exception/UserNotFoundException.java
@@ -1,8 +1,15 @@
 package com.mock.interview.user.domain.exception;
 
+import com.mock.interview.global.exception.CustomClientException;
 import org.springframework.http.HttpStatus;
 
-public class UserNotFoundException extends RuntimeException {
-    private final HttpStatus status = HttpStatus.NOT_FOUND;
-    private final String code = "user.not.found";
+public class UserNotFoundException extends CustomClientException {
+
+    public UserNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "user.not.found");
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause, HttpStatus.NOT_FOUND, "user.not.found");
+    }
 }
