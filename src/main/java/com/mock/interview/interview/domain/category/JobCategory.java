8df207a50a69daeb6f009c9daf248c5d694931f1
Jang <sdsd090811@gmail.com>
2024-01-05 21:01:55 +0900

    feat:  직무, 직군 카테고리 엔티티 추가

---
+++ src/main/java/com/mock/interview/interview/domain/category/JobCategory.java
@@ -0,0 +1,62 @@
+package com.mock.interview.interview.domain.category;
+
+import com.mock.interview.interview.domain.exception.MissingRequiredDepartmentCategoryException;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class JobCategory {
+    @Id
+    @Column(name = "job_category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @Cascade(CascadeType.PERSIST)
+    @JoinColumn(name = "job_parent_id")
+    private JobCategory department;
+
+    /**
+     * 분야 - ex) IT, 기획, 인사 등등
+     * @param name IT, 기획, 인사
+     * @return
+     */
+    public static JobCategory createDepartmentCategory(String name) {
+        JobCategory category = new JobCategory();
+        insertName(category, name);
+        return category;
+    }
+
+    /**
+     * 필드 생성 - 백엔드, 프론트엔드, 안드로이드 등등
+     * @param name 백엔드, 프론트엔드, 안드로이드
+     * @param relationalDepartmentCategory IT, 기획, 인사
+     * @return
+     */
+    public static JobCategory createFieldCategory(String name, JobCategory relationalDepartmentCategory) {
+        if (relationalDepartmentCategory == null) {
+            throw new MissingRequiredDepartmentCategoryException();
+        }
+
+        JobCategory category = new JobCategory();
+        insertName(category, name);
+        category.department = relationalDepartmentCategory;
+        return category;
+    }
+
+    /**
+     * Name은 항상 대문자로 저장
+     */
+    private static void insertName(JobCategory category, String name) {
+        category.name = name.toUpperCase();
+    }
+}
