67bfcbf5b93aa2da18277f7152b4736b7cb5f6bb
Jang <sdsd090811@gmail.com>
2024-01-05 18:35:26 +0900

    feat: 면접 세팅 불러오기 부분 추가

--- src/main/java/com/mock/interview/interview/presentation/web/InterviewController.java
+++ src/main/java/com/mock/interview/interview/presentation/web/InterviewController.java
@@ -2,16 +2,26 @@ package com.mock.interview.interview.presentation.web;
 
 import com.mock.interview.interview.domain.Category;
 import com.mock.interview.interview.presentation.dto.*;
+import com.mock.interview.user.application.CandidateProfileService;
+import com.mock.interview.user.domain.exception.CandidateProfileNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class InterviewController {
+
+    private final CandidateProfileService candidateProfileService;
 
     @GetMapping("/interview/start")
     public String startInterviewPage(
@@ -39,17 +49,29 @@ public class InterviewController {
         return historyDTO;
     }
 
-    @GetMapping("/interview/setting")
-    public String speechPage(Model model) {
+    @GetMapping(value = {"/interview/setting","/interview/setting/{candidateProfileId}"})
+    public String speechPage(
+            Model model, @AuthenticationPrincipal(expression = "id") Long loginId,
+            @PathVariable(required = false, value = "candidateProfileId") Optional<Long> candidateProfileId
+    ) {
         model.addAttribute("headerActiveTap", "interview");
-
         // TODO: Setting DTO를 만들고 Model에 넣어서 타임리프에 th:object로 연결할 것.
         model.addAttribute("categoryList", List.of(Category.IT));
-        model.addAttribute("candidateProfile", new CandidateProfileDto());
         model.addAttribute("interviewDetails", new InterviewDetailsDto());
+
+        CandidateProfileDto profileDto = new CandidateProfileDto();
+        if (candidateProfileId.isPresent() && loginId != null) {
+            profileDto = candidateProfileService.findProfile(candidateProfileId.get(), loginId);
+        }
+        model.addAttribute("candidateProfile", profileDto);
 
         // TODO: Ajax로 분야에 맞는 필드를 가져오도록 수정해야 한다. 일단 임시로 each문 돌린다.
         // TODO: 지금은 면접 설정은 생략하고 기술면접으로 진행한다. 추후 면접 설정도 추가해야 한다.
         return "interview/interview-setting";
+    }
+
+    @GetMapping("/interview/setting/")
+    public String redirectSettingPage() {
+        return "redirect:/interview/setting";
     }
 }
--- src/main/java/com/mock/interview/user/domain/exception/CandidateProfileNotFoundException.java
+++ src/main/java/com/mock/interview/user/domain/exception/CandidateProfileNotFoundException.java
@@ -1,7 +1,9 @@
 package com.mock.interview.user.domain.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class CandidateProfileNotFoundException extends RuntimeException {
     private final HttpStatus status = HttpStatus.NOT_FOUND;
     private final String code = "candidate.profile.not.found";
