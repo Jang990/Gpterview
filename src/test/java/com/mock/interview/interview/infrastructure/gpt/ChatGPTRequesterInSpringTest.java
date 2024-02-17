da12688db4620da6cf44ed45358edcc3d3fc02df
Jang <sdsd090811@gmail.com>
2024-02-17 17:09:22 +0900

    test: OpenAI API 호출 통합 테스트명 변경

--- src/test/java/com/mock/interview/interview/infrastructure/gpt/ChatGPTRequesterInSpringTest.java
+++ src/test/java/com/mock/interview/interview/infrastructure/gpt/ChatGPTRequesterInSpringTest.java
@@ -1,5 +1,6 @@
 package com.mock.interview.interview.infrastructure.gpt;
 
+import com.knuddels.jtokkit.api.Encoding;
 import com.mock.interview.conversation.infrastructure.interview.gpt.ChatGPTRequester;
 import com.mock.interview.conversation.infrastructure.interview.gpt.InterviewAIRequest;
 import com.mock.interview.conversation.infrastructure.interview.gpt.OpenAIResponseConvertor;
@@ -10,6 +11,7 @@ import com.mock.interview.conversation.infrastructure.interview.setting.Intervie
 import com.mock.interview.conversation.presentation.dto.InterviewRole;
 import com.mock.interview.conversation.infrastructure.interview.dto.Message;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,7 +26,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class ChatGPTRequesterTest {
+class ChatGPTRequesterInSpringTest {
 
     @Mock
     RestTemplate openaiRestTemplate;
@@ -32,19 +34,25 @@ class ChatGPTRequesterTest {
     @Mock
     OpenAIResponseConvertor convertor;
 
+    @Mock
+    Encoding stringTokenCounter;
+
+
     @InjectMocks
     ChatGPTRequester requester;
 
-    String prompt = "너는 {IT} 분야 {백엔드} 포지션의 면접관. ";
+    String prompt = "테스트 프롬프트";
 
+    // TODO: 테스트코드 필요.
     @Test
+    @DisplayName("정상처리")
     void test1() {
-        List<Message> history = initHistory();
+        List<Message> history = new ArrayList<>();
         InterviewSetting mockSetting = mock(InterviewSetting.class);
         when(mockSetting.getConcept()).thenReturn(prompt);
 
         ChatGptResponse chatGptResponse = new ChatGptResponse();
-        setTestResponse(chatGptResponse);
+        setTestFunctionResponse(chatGptResponse);
 
         String responseContent = "Hello World";
         when(convertor.convertFunctionResult(any())).thenReturn(new GptFunctionResult(responseContent));
@@ -54,7 +62,29 @@ class ChatGPTRequesterTest {
         Assertions.assertThat(message.getRole()).isEqualTo(InterviewRole.AI.toString());
     }
 
-    private void setTestResponse(ChatGptResponse chatGptResponse) {
+    @Test
+    @DisplayName("정상처리 - 프롬프트 제한 초과")
+    void test2() {
+        String prompt = "테스트 프롬프트";
+        int promptLimit = 1024;
+        List<Message> history = new ArrayList<>();
+        InterviewSetting mockSetting = mock(InterviewSetting.class);
+        when(mockSetting.getConcept()).thenReturn(prompt);
+        when(prompt.length()).thenReturn(promptLimit);
+
+        ChatGptResponse chatGptResponse = new ChatGptResponse();
+        setTestFunctionResponse(chatGptResponse);
+
+        String responseContent = "Hello World";
+        when(convertor.convertFunctionResult(any())).thenReturn(new GptFunctionResult(responseContent));
+        Message message = requester.sendRequest(new InterviewAIRequest(history, mockSetting));
+
+        verify(convertor.convertMessageResult(chatGptResponse));
+        Assertions.assertThat(message.getContent()).isEqualTo(responseContent);
+        Assertions.assertThat(message.getRole()).isEqualTo(InterviewRole.AI.toString());
+    }
+
+    private void setTestFunctionResponse(ChatGptResponse chatGptResponse) {
         List<ChatGptResponse.Choice> list = new ArrayList<>();
         ChatGptResponse.Choice choice = new ChatGptResponse.Choice();
         chatGptResponse.setChoices(list);
@@ -62,12 +92,5 @@ class ChatGPTRequesterTest {
         choice.setMessage(testMessage);
         list.add(choice);
         when(openaiRestTemplate.postForObject(Mockito.nullable(String.class), any(), any())).thenReturn(chatGptResponse);
-    }
-
-    private List<Message> initHistory() {
-        List<Message> history = new ArrayList<>();
-        history.add(new Message(requester.getInterviewerRole(), "Hello User"));
-        history.add(new Message(requester.getUserRole(), "Hello Interviewer"));
-        return history;
     }
 }
