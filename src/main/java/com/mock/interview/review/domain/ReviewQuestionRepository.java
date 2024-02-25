6d16cacb2f84bef44c59ee921600f5152de80271
Jang <sdsd090811@gmail.com>
2024-02-25 10:54:54 +0900

    fix: JpaRepository 제네릭 순서 수정

--- src/main/java/com/mock/interview/review/domain/ReviewQuestionRepository.java
+++ src/main/java/com/mock/interview/review/domain/ReviewQuestionRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewQuestionRepository extends JpaRepository<Long, ReviewQuestion> {
+public interface ReviewQuestionRepository extends JpaRepository<ReviewQuestion, Long> {
+
 }
