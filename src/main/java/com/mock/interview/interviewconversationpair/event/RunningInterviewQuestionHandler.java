1fed6453de585b48aa83a6d13820544dad04ca93
Jang <sdsd090811@gmail.com>
2024-02-28 22:57:39 +0900

    fix: 진행중인 면접에서 질문 생성 시 대화쌍을 만들고 메시지 브로커로 전송하도록 수정

--- src/main/java/com/mock/interview/interviewconversationpair/event/RunningInterviewQuestionHandler.java
+++ src/main/java/com/mock/interview/interviewconversationpair/event/RunningInterviewQuestionHandler.java
@@ -1,5 +1,9 @@
 package com.mock.interview.interviewconversationpair.event;
 
+import com.mock.interview.conversation.domain.ConversationMessageBroker;
+import com.mock.interview.conversation.presentation.dto.InterviewRole;
+import com.mock.interview.conversation.presentation.dto.QuestionInInterviewDto;
+import com.mock.interview.interview.domain.exception.InterviewNotExpiredException;
 import com.mock.interview.interview.domain.exception.InterviewNotFoundException;
 import com.mock.interview.interview.domain.model.Interview;
 import com.mock.interview.interview.infrastructure.InterviewRepository;
@@ -19,14 +23,20 @@ public class RunningInterviewQuestionHandler {
     private final InterviewConversationPairRepository interviewConversationPairRepository;
     private final InterviewRepository interviewRepository;
     private final InterviewQuestionRepository interviewQuestionRepository;
+    private final ConversationMessageBroker conversationMessageBroker;
+
     @EventListener(CreatedRunningInterviewQuestionEvent.class)
     public void handle(CreatedRunningInterviewQuestionEvent event) {
-        Interview interview = interviewRepository.findById(event.interviewId())
-                .orElseThrow(InterviewNotFoundException::new);
-        InterviewQuestion question = interviewQuestionRepository.findById(event.questionId())
+        long interviewId = event.interviewId();
+        long questionId = event.questionId();
+        Interview interview = interviewRepository.findById(interviewId)
+                .orElseThrow(InterviewNotExpiredException::new);
+        InterviewQuestion question = interviewQuestionRepository.findById(questionId)
                 .orElseThrow(InterviewQuestionNotFoundException::new);
 
         InterviewConversationPair conversationPair = InterviewConversationPair.startConversation(interview, question);
         interviewConversationPairRepository.save(conversationPair);
+        conversationMessageBroker.publish(interviewId,
+                new QuestionInInterviewDto(conversationPair.getId(), questionId, InterviewRole.USER.name(), question.getQuestion()));
     }
 }
