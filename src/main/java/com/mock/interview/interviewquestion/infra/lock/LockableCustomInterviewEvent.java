bc13cd16733f72fa9e273923fd483cc2d949b36e
Jang <sdsd090811@gmail.com>
2024-02-29 14:44:57 +0900

    docs: LockableCustomInterviewEvent에 AiResponseProcessingLock를 볼 수 있도록 내용 추가

--- src/main/java/com/mock/interview/interviewquestion/infra/lock/LockableCustomInterviewEvent.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/lock/LockableCustomInterviewEvent.java
@@ -1,6 +1,9 @@
 package com.mock.interview.interviewquestion.infra.lock;
 
-/** 커스텀 인터뷰를 진행하면서 락을 걸고 싶다면 이벤트에 해당 인터페이스를 구현할 것 */
+/**
+ * 커스텀 인터뷰를 진행하면서 락을 걸고 싶다면 이벤트에 해당 인터페이스를 구현할 것
+ * @see AiResponseProcessingLock
+ */
 public interface LockableCustomInterviewEvent {
     Long getInterviewId();
 }
