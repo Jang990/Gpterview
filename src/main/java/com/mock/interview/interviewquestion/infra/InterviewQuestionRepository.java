8c26eb33b5bfe72323b6e408879a4e3ecdb70fc5
Jang <sdsd090811@gmail.com>
2024-03-19 22:58:08 +0900

    feat: 추천 기능을 위한 랜덤 쿼리 추가 - 미흡

--- src/main/java/com/mock/interview/interviewquestion/infra/InterviewQuestionRepository.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/InterviewQuestionRepository.java
@@ -1,7 +1,27 @@
 package com.mock.interview.interviewquestion.infra;
 
 import com.mock.interview.interviewquestion.domain.model.InterviewQuestion;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion, Long> {
+    /*
+    TODO: 다른 방식을 찾아봐야한다.
+        1~100에서 90~100이 유효 데이터라면 90이 선택될 확률이 90%이다. 이정도면 랜덤이 아니다.
+     */
+    @Query("""
+            SELECT iq
+            FROM InterviewQuestion iq
+                JOIN (
+                    SELECT CEIL( RAND() * (SELECT MAX(iq2.id) FROM InterviewQuestion iq2) as max ) as id
+                ) random
+            LEFT JOIN iq.appliedJob ia
+            LEFT JOIN iq.appliedJob.department iad
+            WHERE iq.id >= CAST(random.id as long) AND (ia.name = :department OR iad.name = :department)
+            """)
+    List<InterviewQuestion> findQuestionForRecommend(@Param("department") String department, Pageable pageable);
 }
---
+++ src/test/java/com/mock/interview/interviewquestion/infra/InterviewQuestionRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.mock.interview.interviewquestion.infra;
+
+import com.mock.interview.interviewquestion.domain.model.InterviewQuestion;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@SpringBootTest
+class InterviewQuestionRepositoryTest {
+    @Autowired
+    InterviewQuestionRepository repository;
+
+//    @Test
+    void recommendQueryTest() {
+        List<InterviewQuestion> it = repository.findQuestionForRecommend("IT", PageRequest.of(0, 10));
+        for (InterviewQuestion question : it) {
+            System.out.println(question.getQuestion());
+        }
+    }
+}
