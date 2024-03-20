0b958856a50af15ae1965fd55924ccb828ea2d39
Jang <sdsd090811@gmail.com>
2024-03-20 21:06:24 +0900

    fix: 쿼리 별칭 관련 오류 수정

--- src/main/java/com/mock/interview/interviewquestion/infra/InterviewQuestionRepository.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/InterviewQuestionRepository.java
@@ -5,19 +5,21 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface InterviewQuestionRepository extends JpaRepository<InterviewQuestion, Long> {
     /*
     TODO: 다른 방식을 찾아봐야한다.
         1~100에서 90~100이 유효 데이터라면 90이 선택될 확률이 90%이다. 이정도면 랜덤이 아니다.
      */
-    @Query("""
+    @Query(value = """
             SELECT iq
             FROM InterviewQuestion iq
                 JOIN (
-                    SELECT CEIL( RAND() * (SELECT MAX(iq2.id) FROM InterviewQuestion iq2) as max ) as id
+                    SELECT CEIL( RAND() * (SELECT MAX(iq2.id) FROM InterviewQuestion iq2) ) as id
                 ) random
             LEFT JOIN iq.appliedJob ia
             LEFT JOIN iq.appliedJob.department iad
