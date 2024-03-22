006912ad010bd44326d5170ed0222181f1cdb999
Jang <sdsd090811@gmail.com>
2024-03-22 11:54:30 +0900

    docs: 불필요한 주석 제거

--- src/main/java/com/mock/interview/interviewquestion/infra/ai/prompt/configurator/PromptConfigurationCreator.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/ai/prompt/configurator/PromptConfigurationCreator.java
@@ -12,11 +12,6 @@ public class PromptConfigurationCreator {
 
     public PromptConfiguration create(String promptTemplate, InterviewProfile profile) {
         templateValidator.verify(promptTemplate, profile);
-
-        /*
-        TODO: 여기서 PromptConfiguration 생성자를 default로 바꿨음. 이로 인한 사이드 이펙트 수정해야함.
-            근데 여기 create에서 받는 InterviewProfile을 PromptConfigurationParams을 생성해서 수정할것
-        */
         return new PromptConfiguration(
                 promptTemplate, profile.department(), profile.field(),
                 profile.skills().toString(), profile.experience().toString()
