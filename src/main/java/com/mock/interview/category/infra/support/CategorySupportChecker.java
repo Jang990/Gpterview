5f4bcb246796564fef65eafbc851a9da78633f36
Jang <sdsd090811@gmail.com>
2024-04-02 21:16:41 +0900

    refactor: import문 추가

--- src/main/java/com/mock/interview/category/infra/support/CategorySupportChecker.java
+++ src/main/java/com/mock/interview/category/infra/support/CategorySupportChecker.java
@@ -1,17 +1,22 @@
 package com.mock.interview.category.infra.support;
 
+import com.mock.interview.interviewquestion.infra.ai.prompt.configurator.generator.ITInterviewPromptConfigurator;
+import com.mock.interview.interviewquestion.infra.ai.prompt.configurator.generator.InterviewPromptConfigurator;
+import com.mock.interview.tech.infra.view.CategoryRelatedTechFinder;
+import com.mock.interview.tech.infra.view.ITCategoryRelatedTechFinder;
+
 import java.util.List;
 
 /**
  * 카테고리 기능 추가 방법
  * 1. 현재 카테고리 Checker 구현
  * 2. 다음 클래스들을 구현하면서 Checker를 상속할 것.
- * @see com.mock.interview.tech.infra.view.CategoryRelatedTechFinder
- * @see com.mock.interview.interviewquestion.infra.ai.prompt.configurator.generator.InterviewPromptConfigurator
+ * @see CategoryRelatedTechFinder
+ * @see InterviewPromptConfigurator
  *
  * 예시
- * @see com.mock.interview.tech.infra.view.ITCategoryRelatedTechFinder
- * @see com.mock.interview.interviewquestion.infra.ai.prompt.configurator.generator.ITInterviewPromptConfigurator
+ * @see ITCategoryRelatedTechFinder
+ * @see ITInterviewPromptConfigurator
  */
 public interface CategorySupportChecker {
     List<String> getSupportedCategoryName();
