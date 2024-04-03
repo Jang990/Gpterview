07dacaf45a28ee977f86f70df98155721e081aff
Jang <sdsd090811@gmail.com>
2024-04-03 22:14:58 +0900

    feat: 사용자 카테고리 연결 로직 추가

--- src/main/java/com/mock/interview/user/application/UserService.java
+++ src/main/java/com/mock/interview/user/application/UserService.java
@@ -1,5 +1,10 @@
 package com.mock.interview.user.application;
 
+import com.mock.interview.category.domain.exception.JobCategoryNotFoundException;
+import com.mock.interview.category.domain.model.JobCategory;
+import com.mock.interview.category.domain.model.JobPosition;
+import com.mock.interview.category.infra.JobCategoryRepository;
+import com.mock.interview.category.infra.JobPositionRepository;
 import com.mock.interview.user.domain.model.Users;
 import com.mock.interview.user.infrastructure.UserRepository;
 import com.mock.interview.user.presentation.dto.AccountDto;
@@ -14,10 +19,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final JobCategoryRepository jobCategoryRepository;
+    private final JobPositionRepository jobPositionRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void create(AccountForm accountForm) {
-        Users users = Users.createUser(accountForm.getUsername(), passwordEncoder.encode(accountForm.getPassword()));
+    public void create(AccountForm form) {
+        Users users = Users.createUser(form.getUsername(), passwordEncoder.encode(form.getPassword()));
         userRepository.save(users);
+
+        if (hasPosition(form)) {
+            JobPosition position = jobPositionRepository.findWithCategory(form.getCategoryId(), form.getPositionId())
+                    .orElseThrow(JobCategoryNotFoundException::new);
+            users.linkJob(position.getCategory(), position);
+            return;
+        }
+
+        if (hasCategory(form)) {
+            JobCategory category = jobCategoryRepository.findById(form.getCategoryId())
+                    .orElseThrow(JobCategoryNotFoundException::new);
+            users.linkCategory(category);
+        }
+    }
+
+    private boolean hasCategory(AccountForm form) {
+        return form.getCategoryId() != null;
+    }
+
+    private boolean hasPosition(AccountForm form) {
+        return form.getPositionId() != null;
     }
 }
