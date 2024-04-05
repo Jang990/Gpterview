d582a4f6d123666ad8e1114a387e80c14c94a99e
Jang <sdsd090811@gmail.com>
2024-04-05 16:58:55 +0900

    feat: 메인 페이지 화면 구성 수정

--- src/main/java/com/mock/interview/MainPageController.java
+++ src/main/java/com/mock/interview/MainPageController.java
@@ -28,7 +28,6 @@ public class MainPageController {
         if (users == null) {
             model.addAttribute("activeInterview", new InterviewResponse());
             model.addAttribute("interviewOverviewList", new ArrayList<>());
-            model.addAttribute("reviewOverviewList", new ArrayList<>());
             QuestionPageInitializer.initEmptyQuestionSearchForm(model);
             return "index";
         }
@@ -37,16 +36,8 @@ public class MainPageController {
         List<InterviewOverviewFragment> interviewOverviewList = interviewRepositoryForView
                 .findInterviewOverview(users.getId(), PageRequest.of(0, maxOverviewListSize));
 
-        // TODO: 개발 후 제대로 뺄 것.
-        List<ReviewIndexPageFragment> reviewOverviewList =
-                List.of(
-                        new ReviewIndexPageFragment(1, "멀티 프로세싱과 멀티스레딩을 어떤 상황에서 사용하는 것이 적절할까요?", 10),
-                        new ReviewIndexPageFragment(3, "FIFO 방식의 스케줄링과 라운드 로빈 방식의 스케줄링을 비교해보세요.", 6)
-                );
-
         model.addAttribute("activeInterview", getActiveInterview(users.getId()));
         model.addAttribute("interviewOverviewList", interviewOverviewList);
-        model.addAttribute("reviewOverviewList", reviewOverviewList);
         QuestionPageInitializer.initEmptyQuestionSearchForm(model);
 
         return "index";
