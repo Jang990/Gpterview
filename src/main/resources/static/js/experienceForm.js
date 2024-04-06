c1cde2ad4bfd1c02457fee1e222c518a5efd8821
Jang <sdsd090811@gmail.com>
2024-04-06 17:09:30 +0900

    feat: 회원가입 추가정보 화면 아코디언 추가

--- src/main/resources/static/js/experienceForm.js
+++ src/main/resources/static/js/experienceForm.js
@@ -7,13 +7,13 @@ $(function(){
 
 function addExperienceField() {
     const experienceTextField = createExperienceTextField();
-    experienceForm.append(experienceTextField);
+    experienceForm.prepend(experienceTextField);
 }
 
 
 function createExperienceTextField() {
     return `<div>
-                <div class="mb-5 col">
+                <div class="my-3 col">
                     <div for="intro" class="form-floating">
                         <textarea class="form-control" placeholder="여기에 적어주세요." name="experiences" style="height: 200px"></textarea>
                         <label>직무 관련 경험(프로젝트) (300자 이하) - 생략가능</label>
