4466025dfdf72e7c7b232fe8c39f28e38e5a1fc4
Jang <sdsd090811@gmail.com>
2024-04-07 11:53:14 +0900

    fix: 경험 화면 생성 기능 라벨 수정

--- src/main/resources/static/js/experienceForm.js
+++ src/main/resources/static/js/experienceForm.js
@@ -16,7 +16,7 @@ function createExperienceTextField() {
                 <div class="my-3 col">
                     <div for="intro" class="form-floating">
                         <textarea class="form-control" placeholder="여기에 적어주세요." name="experiences" style="height: 200px"></textarea>
-                        <label>직무 관련 경험(프로젝트) (300자 이하) - 생략가능</label>
+                        <label>자소서 or 이력서 내용 (500자 이하)</label>
                     </div>
                 </div>
             </div>`;
