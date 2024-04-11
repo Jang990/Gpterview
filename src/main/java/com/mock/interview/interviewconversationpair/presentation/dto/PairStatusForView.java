481013165b5c720c5e6cc063ea91f4c593110b1a
Jang <sdsd090811@gmail.com>
2024-04-11 21:27:04 +0900

    fix: 대화쌍 상태와 View에 보여지는 상태 정보 분리

--- src/main/java/com/mock/interview/interviewconversationpair/presentation/dto/PairStatusForView.java
+++ src/main/java/com/mock/interview/interviewconversationpair/presentation/dto/PairStatusForView.java
@@ -3,14 +3,12 @@ package com.mock.interview.interviewconversationpair.presentation.dto;
 import com.mock.interview.interviewconversationpair.domain.model.PairStatus;
 
 public enum PairStatusForView {
-    START, RECOMMENDING, WAITING_AI, WAITING_ANSWER, COMPLETED;
-
-    // TODO: RECOMMENDING 제거할 것
+    EMPTY, ONLY_QUESTION, COMPLETED;
     public static PairStatusForView convert(PairStatus status) {
         return switch (status) {
-            case START -> PairStatusForView.START;
-            case WAITING_QUESTION -> PairStatusForView.WAITING_AI;
-            case WAITING_ANSWER -> PairStatusForView.WAITING_ANSWER;
+            case START -> throw new IllegalArgumentException("서버 데이터 처리 문제"); // START는 보여질 수 없다.
+            case WAITING_QUESTION -> PairStatusForView.EMPTY;
+            case WAITING_ANSWER -> PairStatusForView.ONLY_QUESTION;
             case COMPLETED -> PairStatusForView.COMPLETED;
         };
     }
