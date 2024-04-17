e01a6e3bb821dfdd2f4ef045f86dcafe151f94b0
Jang <sdsd090811@gmail.com>
2024-04-17 15:40:01 +0900

    fix: TOO_MANY_REQUEST를 제외하고 전부 크리티컬 이벤트를 발생하도록 수정

--- src/main/java/com/mock/interview/interviewquestion/domain/ConversationQuestionExceptionHandlingHelper.java
+++ src/main/java/com/mock/interview/interviewquestion/domain/ConversationQuestionExceptionHandlingHelper.java
@@ -3,16 +3,15 @@ package com.mock.interview.interviewquestion.domain;
 import com.mock.interview.global.Events;
 import com.mock.interview.interviewquestion.domain.event.CriticalQuestionSelectionErrorEvent;
 import com.mock.interview.interviewquestion.domain.event.QuestionSelectionErrorEvent;
-import com.mock.interview.interviewquestion.infra.recommend.exception.NotEnoughQuestion;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 
 @Slf4j
 public final class ConversationQuestionExceptionHandlingHelper {
     private ConversationQuestionExceptionHandlingHelper() {}
 
+    /** 지금은 TOO_MANY_REQUEST만 재시도를 요청하고, 다른 예외들은 모의 면접을 종료시킨다. */
     public static void handle(Exception exception, long interviewId, long conversationId) {
         if (exception instanceof RuntimeException e) {
             log.info("AI 요청기 RuntimeException 발생", e);
@@ -25,9 +24,7 @@ public final class ConversationQuestionExceptionHandlingHelper {
     }
 
     private static void handleException(Exception e, long interviewId, long conversationId) {
-        if(e instanceof NotEnoughQuestion)
-            Events.raise(new CriticalQuestionSelectionErrorEvent(e.getClass(), interviewId, conversationId));
-        Events.raise(new QuestionSelectionErrorEvent(e.getClass(), interviewId, conversationId));
+        Events.raise(new CriticalQuestionSelectionErrorEvent(e.getClass(), interviewId, conversationId));
     }
 
     private static void handleRuntimeException(RuntimeException e, long interviewId, long conversationId) {
@@ -39,13 +36,8 @@ public final class ConversationQuestionExceptionHandlingHelper {
     }
 
     private static boolean isCriticalException(Exception exception) {
-        if(exception instanceof HttpServerErrorException)
-            return true;
         if(exception instanceof HttpClientErrorException e)
             return e.getStatusCode() != HttpStatus.TOO_MANY_REQUESTS;
-        if(exception instanceof NotEnoughQuestion)
-            return true;
-
-        return false;
+        return true;
     }
 }
