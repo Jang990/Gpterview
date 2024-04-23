77012285b13edd94c05153fe412a6a0adc4a8dd4
Jang <sdsd090811@gmail.com>
2024-04-23 15:07:19 +0900

    fix: InterviewPromptConfigurator대신 InterviewPromptTemplate을 사용하도록 수정

--- src/main/java/com/mock/interview/interview/infra/prompt/InterviewPromptCreationService.java
+++ src/main/java/com/mock/interview/interview/infra/prompt/InterviewPromptCreationService.java
@@ -2,11 +2,15 @@ package com.mock.interview.interview.infra.prompt;
 
 import com.mock.interview.category.infra.CategoryModuleFinder;
 import com.mock.interview.interview.infra.cache.dto.InterviewInfo;
+import com.mock.interview.interview.infra.cache.dto.InterviewProfile;
+import com.mock.interview.interview.infra.progress.dto.InterviewPhase;
 import com.mock.interview.interview.infra.progress.dto.InterviewProgress;
+import com.mock.interview.interview.infra.prompt.configurator.PromptConfigElements;
+import com.mock.interview.interview.infra.prompt.configurator.PromptConfigurationCreator;
+import com.mock.interview.interview.infra.prompt.configurator.template.InterviewPromptTemplate;
 import com.mock.interview.interviewquestion.domain.AiQuestionCreator;
 import com.mock.interview.interviewquestion.infra.gpt.AISpecification;
 import com.mock.interview.interview.infra.prompt.configurator.PromptConfiguration;
-import com.mock.interview.interview.infra.prompt.configurator.generator.InterviewPromptConfigurator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,20 +19,36 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class InterviewPromptCreationService {
-    private final List<InterviewPromptConfigurator> interviewPromptConfiguratorList;
+    private final List<InterviewPromptTemplate> interviewPromptTemplateList;
     private final PromptCreator promptCreator;
+    private final PromptConfigurationCreator configurationCreator;
     private final AISpecification aiSpec;
 
     public AiPrompt create(InterviewInfo interviewInfo, InterviewProgress interviewProgress, AiQuestionCreator.CreationOption creationOption) {
-        PromptConfiguration promptConfig = createPromptConfig(interviewInfo, interviewProgress);
+        PromptConfiguration promptConfig = createPromptConfig(interviewInfo.profile(), interviewProgress);
         return createPrompt(promptConfig, creationOption);
     }
 
-    private PromptConfiguration createPromptConfig(InterviewInfo interviewInfo, InterviewProgress progress) {
-        InterviewPromptConfigurator configurator = CategoryModuleFinder
-                .findModule(interviewPromptConfiguratorList, interviewInfo.profile().category().getName());
-        return configurator.configStrategy(interviewInfo.profile(), progress);
+    private PromptConfiguration createPromptConfig(InterviewProfile profile, InterviewProgress progress) {
+        String template = getPromptTemplate(progress.phase(), profile.category().getName());
+        PromptConfigElements elements = createPromptElements(profile, progress.getTopicContent());
+        return configurationCreator.create(template, elements);
     }
+
+    private String getPromptTemplate(InterviewPhase phase, String categoryName) {
+        InterviewPromptTemplate promptTemplate = CategoryModuleFinder
+                .findModule(interviewPromptTemplateList, categoryName);
+        return promptTemplate.getTemplate(phase);
+    }
+
+    private PromptConfigElements createPromptElements(InterviewProfile profile, String topic) {
+        return new PromptConfigElements(
+                profile.category().getName(),
+                profile.field().getName(),
+                topic
+        );
+    }
+
 
     private AiPrompt createPrompt(PromptConfiguration promptConfig, AiQuestionCreator.CreationOption creationOption) {
         return switch (creationOption) {
--- src/main/java/com/mock/interview/interview/infra/prompt/configurator/template/InterviewPromptTemplate.java
+++ src/main/java/com/mock/interview/interview/infra/prompt/configurator/template/InterviewPromptTemplate.java
@@ -1,9 +1,18 @@
 package com.mock.interview.interview.infra.prompt.configurator.template;
 
 import com.mock.interview.category.infra.support.CategorySupportChecker;
+import com.mock.interview.interview.infra.progress.dto.InterviewPhase;
 
 public interface InterviewPromptTemplate extends CategorySupportChecker {
     String getPersonal();
     String getExperience();
     String getTechnical();
+
+    default String getTemplate(InterviewPhase phase) {
+        return switch (phase) {
+            case TECHNICAL -> getTechnical();
+            case EXPERIENCE -> getExperience();
+            case PERSONAL -> getPersonal();
+        };
+    }
 }
