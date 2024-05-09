7ff5e957c1c6d680bd941877e6bf465e5c3f96bc
Jang <sdsd090811@gmail.com>
2024-05-09 12:00:43 +0900

    feat: deploy  스크립트 추가

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-echo "> Hello World!"
+REPOSITORY=/home/ec2-user/app/project
+PROJECT_NAME=Gpterview
+
+echo "> Build 파일복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(ps -ef | grep java | awk '{print $2}')
+echo "연재 구동중인 애플리케이션 pid: $CURRUNT_PID"
+
+if [ -z $CURRENT_PID]; then
+        echo "> 현재 구동중인 애플리케이션 없음.";
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 10
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+echo "> JAR Name: $JAR_NAME"
+
+echo "$JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "$JAR_NAME 실행"
+nohup java -jar -Dspring.config.location=classpath:/application-interview.yml,/home/ec2-user/app/application-prod.yml,/home/ec2-user/app/application-openai.yml,/home/ec2-user/app/application-oauth.yml -Dspring.profiles.active=prod $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
