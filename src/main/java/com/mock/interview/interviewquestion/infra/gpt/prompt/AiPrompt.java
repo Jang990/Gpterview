9de604270cc39a7ed9923ef700f5025237c75b2e
Jang <sdsd090811@gmail.com>
2024-05-12 16:27:24 +0900

    fix: topic이 긴 경우 추가정보로 분리해서 요청을 보내도록 수정

--- src/main/java/com/mock/interview/interviewquestion/infra/gpt/prompt/AiPrompt.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/gpt/prompt/AiPrompt.java
@@ -6,8 +6,19 @@ import lombok.ToString;
 @Getter
 @ToString
 public class AiPrompt {
-    private final String prompt;
+    private final String prompt; // 면접관으로 면접을 진행해라. ~기술을 주제로 질문해라.
+    private final String additionalUserInfo; // 너무 긴 추가 정보 - ex) 나는 ~을 했고 ~을 개선했고 ... 뭘 하고 또 뭘하고...
     protected AiPrompt(String prompt) {
         this.prompt = prompt;
+        this.additionalUserInfo = null;
+    }
+
+    protected AiPrompt(String prompt, String additionalUserInfo) {
+        this.prompt = prompt;
+        this.additionalUserInfo = additionalUserInfo;
+    }
+
+    public boolean hasAdditionalUserInfo() {
+        return additionalUserInfo != null && !additionalUserInfo.isBlank();
     }
 }
