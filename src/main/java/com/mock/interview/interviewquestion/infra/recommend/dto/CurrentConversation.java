d27b73a6425d0bcf31d0238a9e0a0603bbca35f0
Jang <sdsd090811@gmail.com>
2024-05-14 19:34:59 +0900

    refactor: beforeQuestionContent -> beforeAnswerContent 리네임

--- src/main/java/com/mock/interview/interviewquestion/infra/recommend/dto/CurrentConversation.java
+++ src/main/java/com/mock/interview/interviewquestion/infra/recommend/dto/CurrentConversation.java
@@ -3,5 +3,5 @@ package com.mock.interview.interviewquestion.infra.recommend.dto;
 
 import java.util.List;
 
-public record CurrentConversation(Long beforeQuestionId, List<String> beforeQuestionContent, String tech, String field) {
+public record CurrentConversation(Long beforeQuestionId, List<String> beforeAnswerContent, String tech, String field) {
 }
