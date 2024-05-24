967e530d9088cf21ba5eed30ab6463a339cce442
Jang <sdsd090811@gmail.com>
2024-05-24 20:50:59 +0900

    refactor: 질문 검색에 있는 공통 코드 helper로 분리

--- src/main/java/com/mock/interview/interviewquestion/presentation/web/list/JobQuestionListWebController.java
+++ src/main/java/com/mock/interview/interviewquestion/presentation/web/list/JobQuestionListWebController.java
@@ -1,13 +1,11 @@
 package com.mock.interview.interviewquestion.presentation.web.list;
 
 import com.mock.interview.interviewquestion.infra.QuestionRepositoryForListView;
-import com.mock.interview.interviewquestion.presentation.dto.QuestionOverview;
 import com.mock.interview.interviewquestion.presentation.dto.QuestionSearchCond;
 import com.mock.interview.interviewquestion.presentation.dto.QuestionSearchOptionsDto;
-import com.mock.interview.interviewquestion.presentation.web.QuestionPageInitializer;
+import com.mock.interview.interviewquestion.presentation.web.list.helper.QuestionSearchHelper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -30,8 +28,7 @@ public class JobQuestionListWebController {
         QuestionSearchOptionsDto searchOptions = QuestionSearchOptionsDto.builder()
                 .categoryIdCond(categoryId)
                 .searchCond(searchCond).build();
-        Page<QuestionOverview> overviewPage = questionRepositoryForListView.findOverviewList(searchOptions, pageable);
-        QuestionPageInitializer.initListPage(model, overviewPage, searchCond, request);
+        QuestionSearchHelper.search(questionRepositoryForListView, model, pageable, request, searchOptions);
         return "question/list";
     }
 
@@ -45,8 +42,7 @@ public class JobQuestionListWebController {
         QuestionSearchOptionsDto searchOptions = QuestionSearchOptionsDto.builder()
                 .positionIdCond(positionId)
                 .searchCond(searchCond).build();
-        Page<QuestionOverview> overviewPage = questionRepositoryForListView.findOverviewList(searchOptions, pageable);
-        QuestionPageInitializer.initListPage(model, overviewPage, searchCond, request);
+        QuestionSearchHelper.search(questionRepositoryForListView, model, pageable, request, searchOptions);
         return "question/list";
     }
 }
--- src/main/java/com/mock/interview/interviewquestion/presentation/web/list/QuestionListWebController.java
+++ src/main/java/com/mock/interview/interviewquestion/presentation/web/list/QuestionListWebController.java
@@ -1,14 +1,11 @@
 package com.mock.interview.interviewquestion.presentation.web.list;
 
-import com.mock.interview.interview.infra.InterviewQuestionRepositoryForView;
 import com.mock.interview.interviewquestion.infra.QuestionRepositoryForListView;
-import com.mock.interview.interviewquestion.presentation.dto.QuestionOverview;
 import com.mock.interview.interviewquestion.presentation.dto.QuestionSearchCond;
 import com.mock.interview.interviewquestion.presentation.dto.QuestionSearchOptionsDto;
-import com.mock.interview.interviewquestion.presentation.web.QuestionPageInitializer;
+import com.mock.interview.interviewquestion.presentation.web.list.helper.QuestionSearchHelper;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 public class QuestionListWebController {
     private final QuestionRepositoryForListView questionRepositoryForListView;
-    private final InterviewQuestionRepositoryForView interviewQuestionRepositoryForView;
 
     @GetMapping("question")
     public String questionListPage(
@@ -30,8 +26,7 @@ public class QuestionListWebController {
             HttpServletRequest request
     ) {
         QuestionSearchOptionsDto searchOptions = QuestionSearchOptionsDto.builder().searchCond(searchCond).build();
-        Page<QuestionOverview> overviewPage = questionRepositoryForListView.findOverviewList(searchOptions, pageable);
-        QuestionPageInitializer.initListPage(model, overviewPage, searchCond, request);
+        QuestionSearchHelper.search(questionRepositoryForListView, model, pageable, request, searchOptions);
         return "question/list";
     }
 
@@ -55,11 +50,10 @@ public class QuestionListWebController {
             @PageableDefault Pageable pageable,
             HttpServletRequest request
     ) {
-        QuestionSearchOptionsDto searchOptionsDto = QuestionSearchOptionsDto.builder()
+        QuestionSearchOptionsDto searchOptions = QuestionSearchOptionsDto.builder()
                 .parentQuestionIdCond(questionId)
                 .searchCond(searchCond).build();
-        Page<QuestionOverview> overviewPage = questionRepositoryForListView.findOverviewList(searchOptionsDto, pageable);
-        QuestionPageInitializer.initListPage(model, overviewPage, searchCond, request);
+        QuestionSearchHelper.search(questionRepositoryForListView, model, pageable, request, searchOptions);
         return "question/list";
     }
 
@@ -71,11 +65,10 @@ public class QuestionListWebController {
             @PageableDefault Pageable pageable,
             HttpServletRequest request
     ) {
-        QuestionSearchOptionsDto searchOptionsDto = QuestionSearchOptionsDto.builder()
+        QuestionSearchOptionsDto searchOptions = QuestionSearchOptionsDto.builder()
                 .ownerIdCond(userId)
                 .searchCond(searchCond).build();
-        Page<QuestionOverview> overviewPage = questionRepositoryForListView.findOverviewList(searchOptionsDto, pageable);
-        QuestionPageInitializer.initListPage(model, overviewPage, searchCond, request);
+        QuestionSearchHelper.search(questionRepositoryForListView, model, pageable, request, searchOptions);
         model.addAttribute("headerActiveTap", "user-question");
         return "question/list";
     }
