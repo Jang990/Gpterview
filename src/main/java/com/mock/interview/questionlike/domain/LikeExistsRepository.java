1cdec678181380ce62c8d3854b91916c2135a202
Jang <sdsd090811@gmail.com>
2024-06-22 15:07:42 +0900

    feat: 질문 상세 페이지에서만 추천이 가능하도록 기능 추가

---
+++ src/main/java/com/mock/interview/questionlike/domain/LikeExistsRepository.java
@@ -0,0 +1,24 @@
+package com.mock.interview.questionlike.domain;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import static com.mock.interview.questionlike.domain.QQuestionLike.questionLike;
+
+@Repository
+@RequiredArgsConstructor
+public class LikeExistsRepository {
+    private final JPAQueryFactory query;
+
+    public boolean isExist(long questionId, long userId) {
+        Integer exist = query.selectOne().
+                from(questionLike)
+                .where(questionLike.question.id.eq(questionId), questionLike.users.id.eq(userId))
+                .fetchFirst();
+
+        System.out.println("==========>"+exist);
+
+        return exist != null;
+    }
+}
