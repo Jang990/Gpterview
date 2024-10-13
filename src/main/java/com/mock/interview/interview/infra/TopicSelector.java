2e998118b9479d70c5735554646d127c28218da6
Jang <sdsd090811@gmail.com>
2024-10-13 15:55:30 +0900

    feat: TopicSelector getTopicList 메소드 추가

--- src/main/java/com/mock/interview/interview/infra/TopicSelector.java
+++ src/main/java/com/mock/interview/interview/infra/TopicSelector.java
@@ -1,12 +1,23 @@
 package com.mock.interview.interview.infra;
 
+import com.mock.interview.interview.infra.cache.dto.InterviewInfo;
+import com.mock.interview.interview.infra.progress.dto.InterviewPhase;
 import com.mock.interview.interview.infra.progress.dto.topic.InterviewTopic;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class TopicSelector {
+    public List<? extends InterviewTopic<?>> getTopicList(InterviewPhase phase, InterviewInfo info) {
+        return switch (phase) {
+            case TECHNICAL -> info.getTechTopics();
+            case EXPERIENCE -> info.getExperienceTopics();
+            case PERSONAL -> Collections.emptyList();
+        };
+    }
+
     public <T extends InterviewTopic<?>> T selectTopic(List<T> topics, double progress) {
         if(topics.isEmpty())
             return null;
--- src/test/java/com/mock/interview/interview/infra/TopicSelectorTest.java
+++ src/test/java/com/mock/interview/interview/infra/TopicSelectorTest.java
@@ -1,5 +1,12 @@
 package com.mock.interview.interview.infra;
 
+import com.mock.interview.category.presentation.dto.response.CategoryResponse;
+import com.mock.interview.interview.infra.cache.dto.InterviewConfig;
+import com.mock.interview.interview.infra.cache.dto.InterviewInfo;
+import com.mock.interview.interview.infra.cache.dto.InterviewProfile;
+import com.mock.interview.interview.infra.progress.dto.InterviewPhase;
+import com.mock.interview.interview.infra.progress.dto.topic.ExperienceTopic;
+import com.mock.interview.interview.infra.progress.dto.topic.InterviewTopic;
 import com.mock.interview.interview.infra.progress.dto.topic.TechTopic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TopicSelectorTest {
 
@@ -17,6 +25,62 @@ class TopicSelectorTest {
             new TechTopic(2L, "Something2"),
             new TechTopic(2L, "Something3")
     );
+
+    @Test
+    @DisplayName("기술 페이즈 리스트")
+    void testTopicList1() {
+        InterviewPhase phase = InterviewPhase.TECHNICAL;
+        List<TechTopic> techTopics = List.of(mock(TechTopic.class));
+        List<ExperienceTopic> experienceTopics = List.of(mock(ExperienceTopic.class));
+
+        List<? extends InterviewTopic<?>> result = selector
+                .getTopicList(phase, mockTopicInfo(techTopics, experienceTopics));
+
+        assertThat(result).isSameAs(techTopics);
+        assertThat(result).isNotSameAs(experienceTopics);
+        assertThat(result.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("경험 페이즈 리스트")
+    void testTopicList2() {
+        InterviewPhase phase = InterviewPhase.EXPERIENCE;
+        List<TechTopic> techs = List.of(mock(TechTopic.class));
+        List<ExperienceTopic> experiences = List.of(mock(ExperienceTopic.class));
+
+        List<? extends InterviewTopic<?>> result = selector
+                .getTopicList(phase, mockTopicInfo(techs, experiences));
+
+        assertThat(result).isSameAs(experiences);
+        assertThat(result).isNotSameAs(techs);
+        assertThat(result.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("인성 페이즈 리스트")
+    void testTopicList3() {
+        InterviewPhase phase = InterviewPhase.PERSONAL;
+        List<TechTopic> techs = List.of(mock(TechTopic.class));
+        List<ExperienceTopic> experiences = List.of(mock(ExperienceTopic.class));
+
+        List<? extends InterviewTopic<?>> result = selector
+                .getTopicList(phase, mockTopicInfo(techs, experiences));
+
+        assertThat(result).isNotSameAs(techs);
+        assertThat(result).isNotSameAs(experiences);
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private InterviewInfo mockTopicInfo(List<TechTopic> techTopics, List<ExperienceTopic> experienceTopics) {
+        return new InterviewInfo(0L,
+                new InterviewProfile(
+                        mock(CategoryResponse.class),
+                        mock(CategoryResponse.class),
+                        techTopics, experienceTopics
+                ),
+                mock(InterviewConfig.class)
+        );
+    }
 
     @Test
     @DisplayName("첫 번째 토픽 선택")
