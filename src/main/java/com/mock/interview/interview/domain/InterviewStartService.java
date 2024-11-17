756746796b0ef05d1f6133b8e7279e60dd882206
Jang <sdsd090811@gmail.com>
2024-11-17 11:13:50 +0900

    feat: 면접의 요소들을 받아서 면접을 생성하는 면접 start 메소드 추가

--- src/main/java/com/mock/interview/interview/domain/InterviewStartService.java
+++ src/main/java/com/mock/interview/interview/domain/InterviewStartService.java
@@ -1,27 +1,44 @@
 package com.mock.interview.interview.domain;
 
-import com.mock.interview.global.RepositoryConst;
+import com.mock.interview.interview.application.dto.InterviewTopicDto;
 import com.mock.interview.interview.domain.exception.InterviewAlreadyInProgressException;
-import com.mock.interview.interview.domain.model.Interview;
+import com.mock.interview.interview.domain.model.*;
 import com.mock.interview.interview.infra.InterviewRepository;
-import com.mock.interview.user.domain.exception.DailyInterviewLimitExceededException;
 import com.mock.interview.user.domain.model.Users;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InterviewStartService {
     private final InterviewTimeHolder timeHolder;
     private final ActiveInterviewFinder activeInterviewFinder;
+    private final InterviewRepository repository;
+    private final InterviewCreator interviewCreator;
+
     public void start(Interview interview, Users users) {
         LocalDateTime now = timeHolder.now();
         if(activeInterviewFinder.hasActiveInterview(users, now))
             throw new InterviewAlreadyInProgressException();
 
         interview.continueInterview(now);
+    }
+
+    public Interview start(
+            CandidateInfo candidateInfo,
+            InterviewTopicDto topics,
+            InterviewTimer timer
+    ) {
+        LocalDateTime now = timeHolder.now();
+        if(activeInterviewFinder.hasActiveInterview(candidateInfo.getUsers(), now))
+            throw new InterviewAlreadyInProgressException();
+
+        Interview interview = interviewCreator.create(candidateInfo, topics, timer);
+        repository.save(interview);
+
+        interview.continueInterview(now);
+        return interview;
     }
 }
--- src/test/java/com/mock/interview/interview/domain/InterviewStartServiceTest.java
+++ src/test/java/com/mock/interview/interview/domain/InterviewStartServiceTest.java
@@ -1,7 +1,11 @@
 package com.mock.interview.interview.domain;
 
+import com.mock.interview.interview.application.dto.InterviewTopicDto;
 import com.mock.interview.interview.domain.exception.InterviewAlreadyInProgressException;
+import com.mock.interview.interview.domain.model.CandidateInfo;
 import com.mock.interview.interview.domain.model.Interview;
+import com.mock.interview.interview.domain.model.InterviewTimer;
+import com.mock.interview.interview.infra.InterviewRepository;
 import com.mock.interview.user.domain.model.Users;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,9 +21,11 @@ import static org.mockito.Mockito.*;
 class InterviewStartServiceTest {
     @Mock InterviewTimeHolder timeHolder;
     @Mock ActiveInterviewFinder activeInterviewFinder;
+    @Mock InterviewRepository repository;
+    @Mock InterviewCreator interviewCreator;
     @InjectMocks InterviewStartService startService;
 
-    @DisplayName("현재 이미 진행중인 면접이 있다면 면접을 진행하지 않는다.")
+    @DisplayName("현재 이미 진행중인 면접이 있다면, 면접을 진행하지 않는다.")
     @Test
     void test1() {
         when(activeInterviewFinder.hasActiveInterview(any(), any())).thenReturn(true);
@@ -29,16 +35,26 @@ class InterviewStartServiceTest {
         );
     }
 
-    @DisplayName("현재 이미 진행중인 면접이 있다면 면접 진행한다.")
+    @DisplayName("이미 진행중인 면접이 없다면, 면접Creator가 생성한 면접을 저장하고 면접을 진행한 후 반환한다.")
     @Test
     void test2() {
+        Interview interview = mock(Interview.class);
+        when(interviewCreator.create(any(), any(), any())).thenReturn(interview);
         when(activeInterviewFinder.hasActiveInterview(any(), any())).thenReturn(false);
 
-        Interview interview = mock(Interview.class);
-        startService.start(interview, mock(Users.class));
+        Interview result = startService.start(
+                mock(CandidateInfo.class),
+                mock(InterviewTopicDto.class),
+                mock(InterviewTimer.class)
+        );
+
+        assertEquals(interview, result);
 
         // TODO: 행위 검증을 상태검증으로 바꿀 방법
         //      InterviewState 같은 것을 필드로 두고 면접을 진행하면 WAIT_TO_QUESTION이 된다면?
+        //      InterviewState가 있고 InterviewState에 따라 도메인 이벤트 호출이 불가능한 상태가 된다면
+        //      굳이 도메인 레벨에서 repository로 save를 호출하지 않아도 된다.
         verify(interview).continueInterview(any());
+        verify(repository).save(any());
     }
 }
